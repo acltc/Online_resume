937eee684ba7cf78a62adbf42800e52a66180fdd
ewilkinsiii <ewilkinsiii@gmail.com>
2015-03-16 20:47:47 -0500

    Twitter

--- app/controllers/online_resumes_controller.rb
+++ app/controllers/online_resumes_controller.rb
@@ -1,4 +1,9 @@
 class OnlineResumesController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+  
   def show
     @student = Student.find(params[:id])
   end
--- app/models/Student.rb
+++ app/models/Student.rb
@@ -1,10 +1,19 @@
 class Student
 
-  attr_reader :first_name, :last_name, :email, :phone_number, :short_bio, :linkendin, :tweeter, :personal_blog_website, :github, :photo
+  attr_reader :first_name, :last_name, :email, :phone_number, :short_bio, :linkedin, :twitter, :blog, :github, :photo, :id
 
   def initialize(hash)
     @first_name = hash["first_name"]
     @last_name = hash["last_name"]
+    @photo = hash["photo"]
+    @email = hash["email"]
+    @phone_number = hash["phone_number"]
+    @linkedin = hash["linkedin"]
+    @short_bio = hash["short_bio"]
+    @github = hash["github"]
+    @blog = hash["blog"]
+    @id = hash["id"]
+    @twitter = hash["twitter"]
   end
 
   def self.find(id)
@@ -14,5 +23,20 @@ class Student
     
   def full_name
     return "#{first_name} #{last_name}"
+  end
+
+  def self.all
+    students_array=Unirest.get("localhost:3001/students.json").body
+    @students= []
+    students_array.each do |student_hash|
+     @students << Student.new(student_hash)
+    end
+  end
+
+  def tweets
+   tweets= []
+   tweets << twitter
+   tweet =tweets[0]
+   return tweet.to_i
   end
 end 
